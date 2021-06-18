e2e038bea7b4b5e22fe3cfc7c4c75c01cd9e230b
Jimmy <jpina@antioch.edu>
2021-06-18 13:25:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[1]}  |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
